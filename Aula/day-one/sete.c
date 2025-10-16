80669d7d56db8646f408a2903c7f2fef09e0f494
JoseEduardo-max <jeduardoff2@gmail.com>
2025-10-16 17:20:22 -0300

    add ate o nono exemplo

---
+++ Aula/day-one/sete.c
@@ -0,0 +1,15 @@
+ # include <stdio.h>
+ # include <stdlib.h>
+ int main () {
+    int i;
+    do{
+        printf("escolha uma opcao:|n);")
+        printf("(1) opcao:1\n);");
+        printf("(2) opcao:2\n);");
+        printf("(3) opcao:3\n);");
+        while ((i < 1) || (i > 3)) {
+            
+        }
+    return 0;
+    
+ }
