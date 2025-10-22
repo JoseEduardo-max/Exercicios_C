475e87d7f3a08fae5b73e5faf54747eff727b525
JoseEduardo-max <jeduardoff2@gmail.com>
2025-10-22 13:30:00 -0300

    fatoração

--- Aula/day-two/two.c
+++ Aula/day-two/two.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int l1, l2, c1, c2;
+    int l1, c1, l2, c2;
 
-    scanf("%d ", &c1);
-    scanf("%d", &l2);
-    scanf("%d", &l1);
-    scanf("%d", &c2);
+    printf("Digite as dimensões da primeira matriz (linhas e colunas): ");
+    scanf("%d %d", &l1, &c1);
 
-    if (c1 !=l2) {
+    printf("Digite as dimensões da segunda matriz (linhas e colunas): ");
+    scanf("%d %d", &l2, &c2);
+
+    if (c1 != l2) {
         printf("Impossível multiplicar as matrizes.\n");
         return 0;
     }
@@ -17,35 +18,34 @@ int main() {
     int matriz2[l2][c2];
     int matriz_resultado[l1][c2];
 
-    printf ("Digite os elementos da primeira matriz (%dx%d):\n", l1, c1);
-
+    printf("Digite os elementos da primeira matriz (%dx%d):\n", l1, c1);
     for (int i = 0; i < l1; i++) {
         for (int j = 0; j < c1; j++) {
             scanf("%d", &matriz1[i][j]);
         }
     }
 
-    printf ("Digite os elementos da segunda matriz (%dx%d):\n", l2, c2);
-
-    for (int i = 0; 1 < l2; i++) {
+    printf("Digite os elementos da segunda matriz (%dx%d):\n", l2, c2);
+    for (int i = 0; i < l2; i++) {
         for (int j = 0; j < c2; j++) {
             scanf("%d", &matriz2[i][j]);
         }
     }
 
     // Inicializa a matriz resultado com zeros
-    for (int i = 0 ; i < l1; i++) {
+    for (int i = 0; i < l1; i++) {
         for (int j = 0; j < c2; j++) {
             matriz_resultado[i][j] = 0;
         }
     }
 
-    // Multiplica as matrizes
     for (int i = 0; i < l1; i++) {
         for (int j = 0; j < c2; j++) {
+            int temp = 0;
             for (int k = 0; k < c1; k++) {
-                matriz_resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+                temp += matriz1[i][k] * matriz2[k][j];
             }
+            matriz_resultado[i][j] = temp;
         }
     }
 
