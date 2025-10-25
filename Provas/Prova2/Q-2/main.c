c2e28244ad5e35af5694c5b434023d9afbd8947a
JoseEduardo-max <jeduardoff2@gmail.com>
2025-10-24 22:55:12 -0300

    fatoração

--- Provas/Prova2/Q-2/main.c
+++ Provas/Prova2/Q-2/main.c
@@ -1,17 +1,24 @@
+/**/
+
 #include <stdio.h>
 
 int main() {
-    int consumo[5] = {100, 110, 105, 115, 120};
+    int consumo[5];
     int variacao[5];
     int i;
 
     variacao[0] = 0; 
 
+    for(i = 0; i < 5; i ++) {
+        printf ("Digite o consumo do dia %d: ", i + 1);
+        scanf ("%d", &consumo[i]);
+    }
+
     for(i = 1; i < 5; i++) {
         variacao[i] = consumo[i] - consumo[i-1];
     }
 
-    printf("Variação percentual:\n");
+    printf("Variacao percentual:\n");
     for(i = 0; i < 5; i++) {
         printf("%d ", variacao[i]);
     }
