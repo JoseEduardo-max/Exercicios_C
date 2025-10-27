053daa8c5d41dedf4883acdfbae1df57d1f8a023
JoseEduardo-max <jeduardoff2@gmail.com>
2025-10-27 13:41:41 -0300

    fatoração da q7

--- Disc/questão7/questão7.c
+++ Disc/questão7/questão7.c
@@ -1,53 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
-struct aluno {
+struct Aluno {
     char nome[50];
-    float notas[3];
+    int notas[3];
     float media;
 };
 
 int main() {
-    struct aluno a1, a2, a3, a4, a5;
-    struct aluno maior;
+    struct Aluno alunos[5];
+    int i, j;
+    float maior_media = 0;
+    int indice_maior = 0;
 
-    printf("Digite o nome do primeiro aluno: ");
-    scanf("%s", a1.nome);
-    printf("Digite as 3 notas de %s:\n", a1.nome);
-    scanf("%f %f %f", &a1.notas[0], &a1.notas[1], &a1.notas[2]);
-    a1.media = (a1.notas[0] + a1.notas[1] + a1.notas[2]) / 3;
+    for (i = 0; i < 5; i++) {
+        printf("Digite o nome do aluno %d: ", i + 1);
+        scanf(" %[^\n]", alunos[i].nome);
 
-    printf("Digite o nome do segundo aluno: ");
-    scanf("%s", a2.nome);
-    printf("Digite as 3 notas de %s:\n", a2.nome);
-    scanf("%f %f %f", &a2.notas[0], &a2.notas[1], &a2.notas[2]);
-    a2.media = (a2.notas[0] + a2.notas[1] + a2.notas[2]) / 3;
+        int soma = 0;
+        for (j = 0; j < 3; j++) {
+            printf("Digite a nota %d de %s: ", j + 1, alunos[i].nome);
+            scanf("%d", &alunos[i].notas[j]);
+            soma += alunos[i].notas[j];
+        }
 
-    printf("Digite o nome do terceiro aluno: ");
-    scanf("%s", a3.nome);
-    printf("Digite as 3 notas de %s:\n", a3.nome);
-    scanf("%f %f %f", &a3.notas[0], &a3.notas[1], &a3.notas[2]);
-    a3.media = (a3.notas[0] + a3.notas[1] + a3.notas[2]) / 3;
+        alunos[i].media = soma / 3.0;
 
-    printf("Digite o nome do quarto aluno: ");
-    scanf("%s", a4.nome);
-    printf("Digite as 3 notas de %s:\n", a4.nome);
-    scanf("%f %f %f", &a4.notas[0], &a4.notas[1], &a4.notas[2]);
-    a4.media = (a4.notas[0] + a4.notas[1] + a4.notas[2]) / 3;
+        if (alunos[i].media > maior_media) {
+            maior_media = alunos[i].media;
+            indice_maior = i;
+        }
 
-    printf("Digite o nome do quinto aluno: ");
-    scanf("%s", a5.nome);
-    printf("Digite as 3 notas de %s:\n", a5.nome);
-    scanf("%f %f %f", &a5.notas[0], &a5.notas[1], &a5.notas[2]);
-    a5.media = (a5.notas[0] + a5.notas[1] + a5.notas[2]) / 3;
+        printf("\n");
+    }
 
-    maior = a1; 
-
-    if (a2.media > maior.media) maior = a2;
-    if (a3.media > maior.media) maior = a3;
-    if (a4.media > maior.media) maior = a4;
-    if (a5.media > maior.media) maior = a5;
-
-    printf("\nA maior media e de %s com %.2f\n", maior.nome, maior.media);
+    printf("\nO aluno com a maior média é: %s\n", alunos[indice_maior].nome);
+    printf("Média: %.2f\n", alunos[indice_maior].media);
 
     return 0;
 }
